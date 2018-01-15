3e0d363fe0f13a5df2174312f68ab3bd0ffa1b5e
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-01-15 12:07:00 -0700

    migrate and seed development table

--- knexfile.js
+++ knexfile.js
@@ -4,16 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      filename: 'postgres://localhost/8080'
-    }
+    connection: 'postgres://localhost/coffeehousedb'
   },
 
   production: {
     client: 'pg',
-    connection: {
-      database: ''
-    }
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
 
 }
--- seeds/01.js
+++ seeds/01.js
@@ -1,13 +1,29 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return knex("coffee")
+    .del()
+    .then(() => {
+      return knex("coffee").insert([
+        {
+          id: 1,
+          name: "Black and Tan",
+          roaster: "Ink",
+          aroma: 3
+        },
+        {
+          id: 2,
+          name: "Holiday Roast",
+          roaster: "Starbucks",
+          aroma: 9
+        },
+        {
+          id: 3,
+          name: "House Quake",
+          roaster: "Denver Coffee",
+          aroma: 6
+        }
+      ])
+    })
+    .then(() => {
+      return knex.raw("ALTER SEQUENCE coffee_id_seq RESTART WITH 4;")
+    })
+}
